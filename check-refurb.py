0c4e54d05895e1ce82646d72ef3225663100b018
Preshant Achuthan <engerium@gmail.com>
2022-08-11 14:53:01 +0800

    Added ability to get apple refurb website based on device

--- check-refurb.py
+++ check-refurb.py
@@ -1,5 +1,18 @@
+import requests
+
+# Constants
+APPLE_REFURB_STORE_URL = "https://www.apple.com/sg/shop/refurbished"
+
+
+def get_apple_refurbished_website(device):
+    page = requests.get(f"{APPLE_REFURB_STORE_URL}/{device}")
+    return page
+
+
 def main():
-    print("hello check-refurb")
+    print("[INFO] Initialised check-refurb script")
+    webpage_text = get_apple_refurbished_website("mac").text
+    print(f"[OUTPUT]\n {webpage_text}")
 
 
 if __name__ == "__main__":
